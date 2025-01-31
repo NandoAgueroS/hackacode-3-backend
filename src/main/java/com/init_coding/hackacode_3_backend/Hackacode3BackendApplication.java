897b234ff6fc9862cc6275acd8702305f0a07c90
NandoAgueroS <nandoaguerojs@gmail.com>
2025-01-31 09:16:52 -0300

    Creación del proyecto Spring Boot

---
+++ src/main/java/com/init_coding/hackacode_3_backend/Hackacode3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.init_coding.hackacode_3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackacode3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackacode3BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/init_coding/hackacode_3_backend/Hackacode3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init_coding.hackacode_3_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackacode3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
