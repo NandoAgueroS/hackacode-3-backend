1e6ec6416f6bb3ee20d4878e56cde3cfa7504e43
NandoAgueroS <nandoaguerojs@gmail.com>
2025-02-15 18:31:54 -0300

    Configura CORS

---
+++ src/main/java/com/init_coding/hackacode_3_backend/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.init_coding.hackacode_3_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed.mapping}")
+    private String allowedMapping;
+
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Value("${cors.allowed.methods}")
+    private String[] allowedMethods;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping(allowedMapping)
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(allowedMethods);
+    }
+
+}
